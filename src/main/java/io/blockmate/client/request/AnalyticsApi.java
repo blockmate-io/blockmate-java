d8b2dddd14dc1f565e0b369b5b1ccd7e9686f8a4
Marek Takac <xtakac@gmail.com>
2022-12-30 14:54:17 +0100

    Add new analytics API methods

--- src/main/java/io/blockmate/client/request/AnalyticsApi.java
+++ src/main/java/io/blockmate/client/request/AnalyticsApi.java
@@ -9,7 +9,7 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import okhttp3.MultipartBody;
 
-import io.blockmate.client.model.GetAnalytics200Response;
+import io.blockmate.client.model.Analytics;
 import io.blockmate.client.model.UserAPIAuthenticateProject400Response;
 import io.blockmate.client.model.UserAPIAuthenticateProject401Response;
 
@@ -20,15 +20,44 @@ import java.util.Map;
 
 public interface AnalyticsApi {
   /**
-   * Get analytics focused on gaming
-   * Get analytics focused on gaming. All empty values are omitted from the response. Values are recalculated once per day.
+   * Get analytics focused on gaming for specified account and provider
+   * Get analytics focused on gaming for specified account and provider. All empty values are omitted from the response. Values are recalculated once per day.
    * @param accountProvider URL value from account_providers method (required)
    * @param accountId Account ID (required)
-   * @return Call&lt;GetAnalytics200Response&gt;
+   * @return Call&lt;Analytics&gt;
    */
   @GET("v1/analytics/{account_provider}/account/{account_id}/stats")
-  Call<GetAnalytics200Response> getAnalytics(
+  Call<Analytics> getAccountAnalytics(
     @retrofit2.http.Path("account_provider") String accountProvider, @retrofit2.http.Path("account_id") String accountId
   );
+
+  /**
+   * Get analytics focused on gaming for whole project
+   * Get analytics focused on gaming for whole project. All empty values are omitted from the response. Values are recalculated once per day.
+   * @return Call&lt;Map&lt;String, Analytics&gt;&gt;
+   */
+  @GET("v1/analytics/project/stats")
+  Call<Map<String, Analytics>> getProjectAnalytics();
+    
+
+  /**
+   * Get analytics focused on gaming for specified provider
+   * Get analytics focused on gaming for specified provider. All empty values are omitted from the response. Values are recalculated once per day.
+   * @param accountProvider URL value from account_providers method (required)
+   * @return Call&lt;Map&lt;String, Analytics&gt;&gt;
+   */
+  @GET("v1/analytics/{account_provider}/stats")
+  Call<Map<String, Analytics>> getProviderAnalytics(
+    @retrofit2.http.Path("account_provider") String accountProvider
+  );
+
+  /**
+   * Get analytics focused on gaming for this user
+   * Get analytics focused on gaming for this user. All empty values are omitted from the response. Values are recalculated once per day.
+   * @return Call&lt;Map&lt;String, Analytics&gt;&gt;
+   */
+  @GET("v1/analytics/user/stats")
+  Call<Map<String, Analytics>> getUserAnalytics();
+    
 
 }
